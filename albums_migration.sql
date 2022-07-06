b98de91ec7601f624507e720a24be7931395fda9
Kelvin <kelvin.daquon.wade@gmail.com>
2022-07-06 14:50:43 -0500

    done

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,8 +6,8 @@ CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(20) NOT NULL,
     name VARCHAR(20) NOT NULL,
-    release_date DECIMAL(4,0) NOT NULL,
-    sales DOUBLE UNSIGNED,
+    release_date INT(4) NOT NULL,
+    sales DOUBLE UNSIGNED NOT NULL,
     genre VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
     );
