7661d08e294705f89d2a1a146a35e244c004c819
Kelvin <kelvin.daquon.wade@gmail.com>
2022-07-08 11:55:14 -0500

    done

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -19,3 +19,15 @@ SELECT * FROM employees WHERE last_name LIKE '%e%'ORDER BY emp_no DESC ;
 SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
 SELECT * FROM employees WHERE last_name LIKE '%q%';
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE  '%qu%';
+
+
+SELECT CONCAT(first_name,' ',last_name)
+    FROM employees WHERE last_name LIKE 'E%e';
+
+SELECT * FROM employees WHERE MONTH(birth_date)=12 AND day(birth_date)=25;
+SELECT * FROM employees WHERE YEAR(hire_date) BETWEEN 1990 AND 1999 AND MONTH(birth_date)=12 AND day(birth_date)=25;
+SELECT * FROM employees WHERE YEAR(hire_date) BETWEEN 1990 AND 1999 AND MONTH(birth_date)=12 AND day(birth_date)=25 ORDER BY birth_date,hire_date DESC;
+SELECT DATEDIFF(curdate(),hire_date),first_name,last_name,hire_date FROM employees WHERE YEAR(hire_date) BETWEEN 1990 AND 1999 AND MONTH(birth_date)=12 AND day(birth_date)=25;
+Select datediff(curdate(),hire_date)FROM employees;
+
+
